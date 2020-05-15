61a5908b9341395a8fd8818c89bc5c814703837e
rizkyrajitha <rajithagunathilake@gmail.com>
2020-05-15 20:43:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,172 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bp = require("body-parser");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+app.use(cors());
+app.use(bp.urlencoded({ extended: false }));
+app.use(bp.json());
+app.use(require("morgan")("dev"));
+
+const RedisClient = redis.createClient({
+  host: "redis-10427.c114.us-east-1-4.ec2.cloud.redislabs.com",
+  port: "10427",
+  password: "cn4NJi5nera8k9RoItdTSW1jhJ4zGjYU",
+});
+
+const port = process.env.PORT || 5000;
+
+const mongodbAPI = "mongodb://127.0.0.1:27017/toodoo"; //process.env.mongourl || require("./config/env").mongodbAPI; //keys.mongouri;
+
+const API = "https://api.opendota.com/api";
+const PLAYERS = [
+  "264853364",
+  "167596568",
+  "326010968",
+  "140732052",
+  "188126754",
+  "172508840", //heshan
+  "210855099",
+];
+
+app.get("/data", (req, res) => {
+  const photosRedisKey = "data:Dota";
+
+  return RedisClient.get(photosRedisKey, (err, data) => {
+    if (!err) {
+      if (data) {
+        return res
+          .status(200)
+          .json({ source: "CACHE", data: JSON.parse(data) });
+      } else {
+        const Data = [];
+
+        var promisearr = PLAYERS.map((element) => {
+          return new Promise((resolve, reject) => {
+            fetch(`${API}/players/${element}`)
+              .then((response) => response.json())
+              .then((profiledata) => {
+                fetch(`${API}/players/${element}/wl`)
+                  .then((response) => response.json())
+                  .then((winlossdata) => {
+                    // console.log(profiledata.data);
+                    // console.log(winlossdata.data);
+
+                    var tempdata = {
+                      ...profiledata,
+                      ...winlossdata,
+                    };
+
+                    resolve(tempdata);
+
+                    console.log(tempdata);
+                    // var ne = [...data, tempdata];
+                    // setdata(ne);
+                    Data.push(tempdata);
+                    // setdata((pre) => {
+                    //   return [...pre, tempdata];
+                    // });
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                    reject(err);
+                  });
+              })
+              .catch((err) => {
+                console.log(err);
+                reject(err);
+              });
+          });
+        });
+
+        console.log("good stuff");
+        console.log(Data);
+
+        Promise.all(promisearr)
+          .then((result) => {
+            console.log(result);
+            RedisClient.setex(photosRedisKey, 3600, JSON.stringify(result));
+            return res
+              .status(200)
+              .json({ source: "OPEN DOTA API", data: result });
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+
+        // if (Data) {
+        //   return res.json({ source: "api", data: Data });
+        // }
+
+        //   client.setex(photosRedisKey, 3600, JSON.stringify(photos));
+
+        // Send JSON response to client
+      }
+    }
+  });
+});
+
+app.get("/oneplayer/:id", (req, res) => {
+  console.log(req.params.id);
+  var element = req.params.id;
+
+  fetch(`${API}/players/${element}`)
+    .then((response) => response.json())
+    .then((profiledata) => {
+      fetch(`${API}/players/${element}/wl`)
+        .then((response) => response.json())
+        .then((winlossdata) => {
+          var tempdata = {
+            ...profiledata,
+            ...winlossdata,
+          };
+
+          console.log(tempdata);
+
+          res.status(200).json(tempdata);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// app.get("/demo", (req, res) => {
+//   const demokey = "123";
+
+//   return RedisClient.get(demokey, (err, data) => {
+//     console.log(err, data);
+//     if (!err) {
+//       if (data) {
+//         console.log("cache kicks in");
+//         return res.status(200).json({ source: "cache", data: data });
+//       } else {
+//         // console.log(Data);
+//         RedisClient.setex(demokey, 3600, "llllllllloooooooooollllllllllll");
+
+//         // Send JSON response to client
+//         console.log("fetced from api");
+//         return res.json({
+//           source: "api",
+//           data: "llllllllloooooooooollllllllllll",
+//         });
+//       }
+//     }
+//   });
+// });
+
+RedisClient.on("error", (err) => {
+  console.log("Error " + err);
+});
+
+app.listen(port, () => {
+  console.log("listning on " + port);
+});
---
+++ client/src/pages/landing/landing.js
@@ -0,0 +1,281 @@
+import React, { useEffect, useState } from "react";
+
+import "./landing.css";
+import Card from "../../components/card";
+import axios from "axios";
+
+const Movie = (props) => {
+  const [data, setdata] = useState([]);
+  const [source, setsource] = useState("");
+  const [modalopen, setmodalopen] = useState(false);
+  const [searchplayerid, setsearchplayerid] = useState("");
+  const [searchplayerdata, setsearchplayerdata] = useState(null);
+  const [isloading, setisloading] = useState(false);
+  const [searchplayererr, setsearchplayererr] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("/data")
+      .then((result) => {
+        // console.log(result.data.data);
+        setdata(result.data.data);
+        setsource(result.data.source);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const fetchoneuser = () => {
+    setsearchplayererr(false);
+    setsearchplayerdata(null);
+    setisloading(true);
+    axios
+      .get(`/oneplayer/${searchplayerid}`)
+      .then((result) => {
+        setisloading(false);
+        if (!result.data.profile.personaname) {
+          setsearchplayererr(true);
+        } else {
+          setsearchplayerdata(result.data);
+        }
+      })
+      .catch((err) => {
+        setisloading(false);
+        setsearchplayererr(true);
+        console.log(err);
+      });
+  };
+
+  const sort = (value) => {
+    switch (value) {
+      case "mmr":
+        // console.log("mmr sort");
+        function mmrcomparator(a, b) {
+          return (
+            parseInt(b.mmr_estimate.estimate, 10) -
+            parseInt(a.mmr_estimate.estimate, 10)
+          );
+        }
+
+        setdata((pre) => {
+          // console.log(pre.sort(mmrcomparator));
+          let newArr = [...pre];
+          return newArr.sort(mmrcomparator);
+        });
+
+        break;
+
+      case "wins":
+        // console.log("wins sort");
+        function winscomparator2(a, b) {
+          return parseInt(b.win, 10) - parseInt(a.win, 10);
+        }
+
+        setdata((pre) => {
+          let newArr = [...pre];
+          return newArr.sort(winscomparator2);
+        });
+
+        break;
+
+      case "winrate":
+        // console.log("wins sort");
+        function winratecomparator3(a, b) {
+          return (
+            100 * (b.win / (b.win + b.lose)) - 100 * (a.win / (a.win + a.lose))
+          );
+        }
+
+        setdata((pre) => {
+          let newArr = [...pre];
+          return newArr.sort(winratecomparator3);
+        });
+
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="maindiv">
+      <header className="navbar">
+        <section className="navbar-section">
+          <a href="#" className="btn btn-link"></a>
+        </section>{" "}
+        <section className="navbar-section">
+          <a href="#" className="btn btn-link"></a>
+        </section>{" "}
+        <section className="navbar-section">
+          <a href="#" className="btn btn-link"></a>
+          PROBROS
+        </section>
+        <section className="navbar-center">
+          <img src="https://img.icons8.com/color/48/000000/dota.png" />
+        </section>{" "}
+        <section className="navbar-section">
+          <a href="#" className="btn btn-link">
+            {"                 "}
+          </a>
+        </section>{" "}
+        <section className="navbar-section">
+          <a href="#" className="btn btn-link">
+            {"                 "}
+          </a>
+          NETWORK
+        </section>
+        <section className="navbar-section">
+          <span className="card-subtitle">Served via {source}</span>
+          <a href="#" className="btn btn-link"></a>{" "}
+        </section>
+      </header>
+
+      <div className=" container">
+        <div class="columns">
+          <div class="column col-3">
+            <div className="form-group">
+              <select
+                onChange={(e) => {
+                  console.log(e.target.value);
+                  sort(e.target.value);
+                }}
+                className="form-select"
+              >
+                <option>Sort by</option>
+                <option value="mmr">MMR</option>
+                <option value="wins">WINS</option>
+                <option value="winrate">WINRATE</option>
+              </select>
+            </div>
+          </div>{" "}
+          <button
+            className="btn btn-primary"
+            onClick={() => setmodalopen(true)}
+          >
+            Find custom player
+          </button>
+        </div>
+
+        <div className={modalopen ? "modal active" : "modal"} id="modal-id">
+          <a
+            href="#close"
+            onClick={() => setmodalopen(false)}
+            className="modal-overlay"
+            aria-label="Close"
+          ></a>
+          <div className="modal-container">
+            <div className="modal-header">
+              <a
+                href="#close"
+                onClick={() => setmodalopen(false)}
+                className="btn btn-clear float-right"
+                aria-label="Close"
+              ></a>
+              <div className="modal-title h5">Search custom player</div>
+            </div>
+            <div className="modal-body">
+              <div className="content">
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    fetchoneuser();
+                  }}
+                >
+                  <div class="form-group">
+                    <label class="form-label" for="input-example-1">
+                      Steam 32 ID{" "}
+                      <a
+                        // className=" tooltip"
+                        // data-tooltip="Lorem ipsum dolor sit amet"
+                        href="https://steamid.xyz/"
+                        target="_blank"
+                      >
+                        if you don't know Steam 32 ID
+                      </a>
+                    </label>
+
+                    {/* <div className="input-group"> */}
+                    <div className="has-icon-right">
+                      <input
+                        class="form-input"
+                        type="text"
+                        id="input-example-1"
+                        placeholder="Steam 32 ID"
+                        minLength="9"
+                        required
+                        onChange={(e) => setsearchplayerid(e.target.value)}
+                      />{" "}
+                      <i hidden={!isloading} className="form-icon loading"></i>
+                    </div>
+
+                    {/* <button
+                      type="submit"
+                      className="btn input-group-btn"
+                      // onClick={() => fetchoneuser()}
+                    >
+                      Search
+                    </button> */}
+                  </div>
+                  {/* </div> */}
+                </form>
+                {searchplayererr && (
+                  <span className="text-error">error user not found</span>
+                )}
+                {searchplayerdata && (
+                  <Card
+                    username={searchplayerdata.profile.personaname}
+                    avatarurl={searchplayerdata.profile.avatarfull}
+                    mmr={searchplayerdata.mmr_estimate.estimate}
+                    profileurl={searchplayerdata.profile.profileurl}
+                    dotaplus={searchplayerdata.profile.plus}
+                    wins={searchplayerdata.win}
+                    loses={searchplayerdata.lose}
+                    countrycode={searchplayerdata.profile.loccountrycode}
+                  />
+                )}
+              </div>
+            </div>
+            <div className="modal-footer"></div>
+          </div>
+        </div>
+
+        <div className="cardstack">
+          {data.length &&
+            data.map((ele) => {
+              return (
+                <Card
+                  username={ele.profile.personaname}
+                  avatarurl={ele.profile.avatarfull}
+                  mmr={ele.mmr_estimate.estimate}
+                  profileurl={ele.profile.profileurl}
+                  dotaplus={ele.profile.plus}
+                  wins={ele.win}
+                  loses={ele.lose}
+                  countrycode={ele.profile.loccountrycode}
+                />
+              );
+            })}
+        </div>
+      </div>
+
+      <div className="footer">
+        <a href="github.com" className="footericon">
+          <img
+            className="img-responsive"
+            src="https://img.icons8.com/ios-filled/100/000000/github.png"
+          />
+        </a>
+        <a className="footericon">
+          <img
+            className="img-responsive dicodbanner"
+            src="https://discordapp.com/api/guilds/464811671773249536/widget.png?style=banner1"
+          />{" "}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
